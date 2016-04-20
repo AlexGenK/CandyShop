62a302e503278044d1e72da348497f4d3cbfa12e
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-04-20 16:45:34 +0300

    add self-create DB

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ require 'sinatra/reloader'
 require 'pony'
 require 'sqlite3'
 
+# процедура инициализации
+configure do
+	# подключаемся, а при отсутствии создаем БД
+	@db=SQLite3::Database.new "./public/sql/candyshop.db"
+	@db.execute "CREATE TABLE IF NOT EXISTS 'Users' ('Id' INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE, 
+													'Name' VARCHAR, 
+													'Phone' VARCHAR, 
+													'Date' VARCHAR, 
+													'Candy' VARCHAR, 
+													'Color' VARCHAR)"
+end
+
 # главная страница
 get '/' do
 	erb "<h1> Hello! We are GREAT!!! </h1>"
@@ -48,9 +60,7 @@ post '/visit' do
 	if @error!=""
 		erb :visit
 	else
-		db=SQLite3::Database.new "./public/sql/candyshop.sqlite"
-		db.execute "INSERT INTO users (Name, Phone, Date, Candy, Color) VALUES ('#{@username}', '#{@phone}', '#{@data}', '#{@candy}', '#{@colorpicker}')"
-		db.close
+		@db.execute "INSERT INTO users (Name, Phone, Date, Candy, Color) VALUES ('#{@username}', '#{@phone}', '#{@data}', '#{@candy}', '#{@colorpicker}')"
 		erb "<h2>Congratulations! You make an appointment.</h2>"
 	end
 end
